41cb57d44f0aa906d62e51c7fe0dd75cc8313c13
Olivia Jarman <ocjarman@gmail.com>
2022-09-01 11:24:23 -0400

    adding functionality

--- index.js
+++ index.js
@@ -29,31 +29,78 @@ const element = $("#workrelated")[0];
 // pageHeroElement.addEventListener("click", () => console.log("hello!"))
 
 
-//click event
-
-let preferred = $('#preferred')[0]
-let avoid = $('#avoid')[0]
-let chemicals = $('#chemicals')[0]
-
-let makeVisible = $('.invisible')[0]
-let makeVisible1 = $('.invisible1')[0]
-let makeVisible2 = $('.invisible2')[0]
-let preferredheader = $('#preferredheader')[0]
 
 
-//when the preferred box is clicked, the .invisible class will appear
-preferred.addEventListener("click", () => makeVisible.style.display = 'block')
-avoid.addEventListener("click", () => makeVisible1.style.display = 'block' )
+//////////////******HOME PAGE******/////////////
+let generalAdviceButton = document.getElementById('generaladvice') //div to be clicked
+let makeMeVisible = document.querySelector('.hidden')
+let shopAdviceButton = document.getElementById('wheretobuy')
+let notAmazonButton = document.getElementById('notamazon')
+
+
+generalAdviceButton.addEventListener("click", toggleSection1)
+shopAdviceButton.addEventListener("click", toggleSection2)
+notAmazonButton.addEventListener("click", toggleSection3)
+
+
+function toggleSection1() {
+    let elementToShow = document.getElementById("generaladvice");
+    if (elementToShow.children[1].className === "hidden") {
+        elementToShow.children[1].className = 'display' 
+    } else {
+        elementToShow.children[1].className = "hidden"    }
+}
+
+function toggleSection2() {
+    let elementToShow = document.getElementById("wheretobuy");
+    if (elementToShow.children[1].className === "hidden") {
+        elementToShow.children[1].className = 'display' 
+    } else {
+        elementToShow.children[1].className = "hidden"    }
+}
+
+function toggleSection3() {
+    let elementToShow = document.getElementById("notamazon");
+    if (elementToShow.children[1].className === "hidden") {
+        elementToShow.children[1].className = 'display' 
+    } else {
+        elementToShow.children[1].className = "hidden"    }
+}
+
+//     if (
+//         event.target.style.display = 'none'
+//     } else {
+//         event.target.style.display === 'block' 
+// }
 
 
 
-let avoidHeader = $('#avoidheader')[0]
-// avoidHeader.addEventListener("click", () => makeVisible3.style.display = 'none' )
 
 
 
-// not working!!!!!!!!
-let clickMe = $('#generaladvice')[0]; //div to be clicked
-let makeMeVisible = $('#hidden')[0]; //text to unhide
 
-clickMe.addEventListener("click", () => makeMeVisible.style.display = 'block')
+
+
+
+// //////////////******PRODUCT PAGE******/////////////
+// //click event
+
+// let preferred = $('#preferred')[0]
+// let avoid = $('#avoid')[0]
+// let chemicals = $('#chemicals')[0]
+
+// let makeVisible = $('.invisible')[0]
+// let makeVisible1 = $('.invisible1')[0]
+// let makeVisible2 = $('.invisible2')[0]
+// let preferredheader = $('#preferredheader')[0]
+
+
+// //when the preferred box is clicked, the .invisible class will appear
+// preferred.addEventListener("click", () => makeVisible.style.display = 'block')
+// avoid.addEventListener("click", () => makeVisible1.style.display = 'block' )
+
+
+
+// let avoidHeader = $('#avoidheader')[0]
+// // avoidHeader.addEventListener("click", () => makeVisible3.style.display = 'none' )
+
