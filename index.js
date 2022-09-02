1b400787af71332a898056a90faebc1e3420c888
Olivia Jarman <ocjarman@gmail.com>
2022-09-02 10:45:41 -0400

    added background color change button, added karat clicker

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log('hello world!')
+console.log("hello world!");
 
 const element = $("#workrelated")[0];
 // element.innerHTML = "Werk'"
@@ -24,63 +24,72 @@ const element = $("#workrelated")[0];
 //set Timeout -- takes an id element
 // adds the properties from the class .invisble and has them happen once the time passes (3 seconds, 3000 ms)
 
-
 // const pageHeroElement = $("#pagehero")[0];
 // pageHeroElement.addEventListener("click", () => console.log("hello!"))
 
-
-
-
 //////////////******HOME PAGE******/////////////
-let generalAdviceButton = document.getElementById('generaladvice') //div to be clicked
-let makeMeVisible = document.querySelector('.hidden')
-let shopAdviceButton = document.getElementById('wheretobuy')
-let notAmazonButton = document.getElementById('notamazon')
-//hello test
+let generalAdviceButton = document.getElementById("generaladvice"); //div to be clicked
+let makeMeVisible = document.querySelector(".hidden");
+let shopAdviceButton = document.getElementById("wheretobuy");
+let notAmazonButton = document.getElementById("notamazon");
 
-generalAdviceButton.addEventListener("click", toggleSection1)
-shopAdviceButton.addEventListener("click", toggleSection2)
-notAmazonButton.addEventListener("click", toggleSection3)
-
+generalAdviceButton.addEventListener("click", toggleSection1);
+shopAdviceButton.addEventListener("click", toggleSection2);
+notAmazonButton.addEventListener("click", toggleSection3);
 
 function toggleSection1() {
-    let elementToShow = document.getElementById("generaladvice");
-    if (elementToShow.children[1].className === "hidden") {
-        elementToShow.children[1].className = 'display' 
-    } else {
-        elementToShow.children[1].className = "hidden"    }
+  let elementToShow = document.getElementById("generaladvice");
+  if (elementToShow.children[1].className === "hidden") {
+    elementToShow.children[1].className = "display";
+  } else {
+    elementToShow.children[1].className = "hidden";
+  }
 }
 
 function toggleSection2() {
-    let elementToShow = document.getElementById("wheretobuy");
-    if (elementToShow.children[1].className === "hidden") {
-        elementToShow.children[1].className = 'display' 
-    } else {
-        elementToShow.children[1].className = "hidden"    }
+  let elementToShow = document.getElementById("wheretobuy");
+  if (elementToShow.children[1].className === "hidden") {
+    elementToShow.children[1].className = "display";
+  } else {
+    elementToShow.children[1].className = "hidden";
+  }
 }
 
 function toggleSection3() {
-    let elementToShow = document.getElementById("notamazon");
-    if (elementToShow.children[1].className === "hidden") {
-        elementToShow.children[1].className = 'display' 
-    } else {
-        elementToShow.children[1].className = "hidden"    }
+  let elementToShow = document.getElementById("notamazon");
+  if (elementToShow.children[1].className === "hidden") {
+    elementToShow.children[1].className = "display";
+  } else {
+    elementToShow.children[1].className = "hidden";
+  }
 }
 
-//     if (
-//         event.target.style.display = 'none'
-//     } else {
-//         event.target.style.display === 'block' 
-// }
+////////////******background color change******/////////// */
+let backgroundButton = document.getElementById("backgroundClicker");
+let elementToChange = document.querySelector("body");
 
+backgroundButton.addEventListener("click", newColor);
 
+function newColor() {
+  let randomNum = Math.floor(Math.random() * 360);
+  elementToChange.style.backgroundColor = `hsl(${randomNum}, 100%, 50%)`;
+}
 
+/////////////******Karat Clicker!!!********///////////////
+let chihuahuaCount = 0;
 
+const divToBeClicked = document.getElementById("karatClicker");
 
-
-
-
-
+const numberToChange = document.getElementById("numOfChiChis");
+function moreChihuahuas() {
+  chihuahuaCount += 1;
+  console.log(numberToChange.innerText);
+  numberToChange.innerText = `${chihuahuaCount}`;
+  // changeChiChi();
+  // console.log(chihuahuaCount);
+}
+// when div is clicked, run more chihuahuas
+divToBeClicked.addEventListener("click", moreChihuahuas);
 
 // //////////////******PRODUCT PAGE******/////////////
 // //click event
@@ -94,13 +103,9 @@ function toggleSection3() {
 // let makeVisible2 = $('.invisible2')[0]
 // let preferredheader = $('#preferredheader')[0]
 
-
 // //when the preferred box is clicked, the .invisible class will appear
 // preferred.addEventListener("click", () => makeVisible.style.display = 'block')
 // avoid.addEventListener("click", () => makeVisible1.style.display = 'block' )
 
-
-
 // let avoidHeader = $('#avoidheader')[0]
 // // avoidHeader.addEventListener("click", () => makeVisible3.style.display = 'none' )
-
